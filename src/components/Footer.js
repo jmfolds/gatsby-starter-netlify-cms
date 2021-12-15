1eaa8b3f7813528eec1b3ebf38553e52835f707f
jmfolds <2747251+jmfolds@users.noreply.github.com>
2021-12-15 11:18:07 -0700

    add admin link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
+import { Link } from "gatsby";
 import * as React from "react";
 
 const Footer = class extends React.Component {
   render() {
     return (
     <footer style={{ textAlign: 'center', margin: '1rem'}}>
-      All art, photos, and text are copyrighted © 2009-2019 by the Wichita
-      Sculptors Guild or the individual artist/gallery/studio/business
-      represented on this web site. Do not reproduce without written permission.
+      All art, photos, and text are copyrighted © 2009-2019 by the Wichita Sculptors Guild or the individual artist/gallery/studio/business represented on this web site. Do not reproduce without written permission.
+      <Link to="/admin">Admin</Link>
     </footer>
     );
   }
