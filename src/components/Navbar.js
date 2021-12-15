c7cd7954db3e442f2d2f8670e2fa9872b039fdd6
jmfolds <2747251+jmfolds@users.noreply.github.com>
2021-12-14 23:30:05 -0700

    update navbar / styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql, StaticQuery } from 'gatsby';
 
-const Navbar = class extends React.Component {
+const NavbarTemplate = class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -11,32 +11,82 @@ const Navbar = class extends React.Component {
   }
 
   render() {
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
     return (
       <nav
-        className="navbar is-transparent"
+        className="navbar"
         role="navigation"
         aria-label="main-navigation"
+        style={{ display: 'flex' }}
       >
-      <div className="navbar-start has-text-centered">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/artists">
-          Artists
-        </Link>
-        <Link className="navbar-item" to="/calendar">
-          Calendar
-        </Link>
-        <Link className="navbar-item" to="/resources">
-          Resources
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-      </div>
+        <ul>
+          <li>
+            <Link className="navbar-item" to="/about">
+              About
+            </Link></li>
+          <li>
+            <Link className="navbar-item" to="/artists">
+              Artists
+            </Link>
+            <ul class="dropdown">
+            {posts &&
+              posts.map(({ node: post }) => {
+                return (
+                <li>
+                  <Link to={post.fields.slug}>
+                    {post.frontmatter.name}
+                  </Link>
+                </li>)
+              })
+            }
+            </ul>
+          </li>
+          <li>
+            <Link className="navbar-item" to="/calendar">
+                Calendar
+            </Link>
+          </li>
+          <li>
+            <Link className="navbar-item" to="/resources">
+              Resources
+            </Link>
+          </li>
+        </ul>
       </nav>
     );
   }
 };
 
-export default Navbar;
+// export default Navbar;
+
+
+export default function Navbar() {
+  return (
+    <StaticQuery
+      query={graphql`
+        query NavbarArtistRollQuery {
+          allMarkdownRemark(
+            filter: { frontmatter: { templateKey: { eq: "artist-post" } } }
+          ) {
+            edges {
+              node {
+                excerpt(pruneLength: 400)
+                id
+                fields {
+                  slug
+                }
+                frontmatter {
+                  name
+                  templateKey
+                  description
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data, count) => <NavbarTemplate data={data} count={count} />}
+    />
+  );
+}
