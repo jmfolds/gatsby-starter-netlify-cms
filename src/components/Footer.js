9a41d8d1271d8fadfba601690df3aa2b76293e02
jmfolds <2747251+jmfolds@users.noreply.github.com>
2022-01-10 20:43:46 -0700

    style tweak

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = class extends React.Component {
     return (
     <footer className="p-3 m-0 mt-3" style={{ textAlign: 'center', margin: '1rem', backgroundColor: '#333' }}>
       <p className="small m-0">All art, photos, and text are copyrighted © 2009-2022 by the Wichita Sculptors Guild or the individual artist/gallery/studio/business represented on this web site.</p>
-      <p className="small"> Do not reproduce without written permission.</p>
+      <p className="small m-0"> Do not reproduce without written permission.</p>
     </footer>
     );
   }
